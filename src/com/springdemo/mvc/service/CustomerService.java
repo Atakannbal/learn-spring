290cc093910ebcfe08e54f2dff7ddcd20d684b19
Atakan Bal <atakannbal@gmail.com>
2022-10-09 21:10:51 +0300

    fix: add missing service annotation

--- src/com/springdemo/mvc/service/CustomerService.java
+++ src/com/springdemo/mvc/service/CustomerService.java
@@ -2,8 +2,11 @@ package com.springdemo.mvc.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.springdemo.mvc.entity.Customer;
 
+@Service
 public interface CustomerService {
 
     public List<Customer> getCustomers();
