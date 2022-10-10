02b800c64b2f950392c1e63802272c10e36bf00f
Atakan Bal <atakannbal@gmail.com>
2022-10-10 12:39:02 +0300

    feat: delete customer

--- src/com/springdemo/mvc/dao/CustomerDAOImpl.java
+++ src/com/springdemo/mvc/dao/CustomerDAOImpl.java
@@ -57,4 +57,21 @@ public class CustomerDAOImpl implements CustomerDAO {
 
         return theCustomer;
     }
+
+    @Override
+    public Object deleteCustomer(int theId) {
+
+        // get the current hibernate session
+        Session currentSession = sessionFactory.getCurrentSession();
+
+        // delete the object with primary key
+        Query theQuery = currentSession.createQuery("delete from Customer where id=:customerId");
+        theQuery.setParameter("customerId", theId);
+
+        theQuery.executeUpdate();
+
+        return null;
+    }
+
+    
 }
