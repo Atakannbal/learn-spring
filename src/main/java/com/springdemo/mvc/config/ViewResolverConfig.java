72c9baf24bd67bf3505bc4e6127875c575fe0f80
Atakan Bal <atakannbal@gmail.com>
2023-06-11 20:40:12 +0300

    use Configuration class for viewResolver bean

---
+++ src/main/java/com/springdemo/mvc/config/ViewResolverConfig.java
@@ -0,0 +1,17 @@
+package com.springdemo.mvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class ViewResolverConfig {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
